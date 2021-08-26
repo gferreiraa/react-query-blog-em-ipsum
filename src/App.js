79eba04356cb000a76ccea73491d50db89ca2982
Getúlio Rafael Ferreira <gferreiraa@gmail.com>
2021-08-26 02:26:15 -0300

    feat: Base structure project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Posts } from "./Post";
+import "./App.css";
+
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+/* 
+  First step is create a Client Provider, import { QueryClient and QueryClientProvider }
+  
+*/
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // provide React Query client to App
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Blog Posts</h1>
+        <Posts />
+      </div>
+    </QueryClientProvider>
   );
 }
 
