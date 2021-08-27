2ad7d9575524168e6daaee56bbe5f3ffd19afc9f
Rafael Ferreira <rferreira@hive9.com>
2021-08-27 00:21:25 -0300

    feat: Pagination

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Posts } from "./Post";
 import "./App.css";
 
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 /* 
   First step is create a Client Provider, import { QueryClient and QueryClientProvider }
@@ -18,6 +19,7 @@ function App() {
         <h1>Blog Posts</h1>
         <Posts />
       </div>
+      <ReactQueryDevtools/>
     </QueryClientProvider>
   );
 }
