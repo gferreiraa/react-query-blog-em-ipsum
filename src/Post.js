6096120251138fc171ba492d95e894fc4dc81589
Getúlio Rafael Ferreira <gferreiraa@gmail.com>
2021-08-30 00:02:33 -0300

    feat: Finishing blog-em ipsum app integrations

--- src/Post.js
+++ src/Post.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useQuery } from "react-query";
+import { useState, useEffect } from "react";
+import { useQuery, useQueryClient } from "react-query";
 
 import { PostDetail } from "./PostDetails";
 const maxPostPage = 10;
@@ -15,15 +15,25 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if(currentPage < maxPostPage) {
+      const nextPage = currentPage + 1
+      queryClient.prefetchQuery(["posts", nextPage], () => fetchPosts(nextPage))
+    }
+  }, [currentPage, queryClient])
+
   // replace with useQuery
-  const { data, isError, isLoading, error } = useQuery(
+  const { data, isError, error, isFetching } = useQuery(
     ["posts", currentPage],
     () => fetchPosts(currentPage),
     {
       staleTime: 2000,
+      keepPreviousData: true
     }
   );
-  if (isLoading) return <div>Loading</div>;
+  if (isFetching) return <div>Fetching...</div>;
   if (isError)
     return (
       <div>
